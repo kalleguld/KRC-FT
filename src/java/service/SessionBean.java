dc27b0bb04c54ea08110dc74e205452573bb0445
Christian Sigersted <sigersted7@hotmail.com>
2014-04-08 09:59:15 +0200

    SessionBean næsten færdig

--- src/java/service/SessionBean.java
+++ src/java/service/SessionBean.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package service;
 
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import model.Category;
+import model.Post;
 import model.PostThread;
 import model.User;
 
@@ -19,16 +20,26 @@ import model.User;
 @Named
 @SessionScoped
 public class SessionBean implements Serializable {
+    //associeringer 
+    private ApplicationBean application;
+
+    //Bruges til at hente informationer omkring der hvor man er nu
+    private String currentTopic;    
     private User currentUser;
-    private String threadTopic;
     private String postText;
     private Category currentCategory;
+    //Bruges til at lave nye threads
+    private String threadTopic;
     private PostThread currentThread;
+    //Bruges til at lave nye Posts
+    private String currentText;
     private Post currentPost;
+    //Bruges til at login og out samt oprette nye Users
     private String loginName;
     private String loginPassword;
-    private ApplicationBean application;
+    //Bruges til at slette en bestemt bruger
     private User selectedUser;
+    
 
     public User getCurrentUser() {
         return currentUser;
@@ -93,31 +104,93 @@ public class SessionBean implements Serializable {
     public void setLoginPassword(String loginPassword) {
         this.loginPassword = loginPassword;
     }
-    
-    public void createUser() {
-        User newUser = new User(this.loginName, this.loginPassword);
-        application.addUser(newUser);
+
+    public User getSelectedUser() {
+        return selectedUser;
     }
-    
+
+    public void setSelectedUser(User selectedUser) {
+        this.selectedUser = selectedUser;
+    }
+
+    public String getCurrentTopic() {
+        return currentTopic;
+    }
+
+    public void setCurrentTopic(String currentTopic) {
+        this.currentTopic = currentTopic;
+    }
+
+    public String getCurrentText() {
+        return currentText;
+    }
+
+    public void setCurrentText(String currentText) {
+        this.currentText = currentText;
+    }
+
+    public void createUser() {
+        application.createUser(this.loginName, this.loginPassword);
+    }
+
     public void deleteUser() {
-        if(currentUser.isAdmin()) {
+        if (currentUser.isAdmin()) {
             application.removeUser(selectedUser);
         }
     }
-    
+
     public void deleteSelf() {
         application.removeUser(currentUser);
     }
-    
-        public String login() {
-        User validUser = application.userCheck(currentUser);
-        if (validUser != null) {
-            validUser = null; // for at spare plads
+
+    public String login() {
+        User validUser = application.getUserByName(this.loginName);
+        if (validUser.doesPasswordMatch(loginPassword)) {
+            currentUser = validUser; // for at spare plads
             return null;
         } else {
-            currentUser = new User("",""); // for at nulstille felterne
-            return "error";
+            currentUser = null; // for at nulstille felterne
+            return null;
         }
     }
+    
+    public String logout() {
+        String navigate = null;
+        if(currentUser != null) {
+            currentUser = null;
+            navigate = "index";
+        } else navigate = null;
+        
+        return navigate;
+    }
+    
+    public void createThread() {
+        application.createThread(this.threadTopic, this.currentCategory, this.currentUser);
+    }
+    
+    public void deleteThread() {
+        //TODO
+    }
 
+    public void createCategory(){
+        application.createCategory(this.currentUser, this.threadTopic);
+    }
+    
+    public void deleteCategory(){
+        //TODO
+    }
+    
+    public void createPost() {
+        if(currentUser != null)
+        application.createPost(currentThread, postText, currentUser);
+    }
+
+    public void deletePost() {
+        
+    }
+    
+    public void editPost() {
+        if(currentUser == currentPost.getUser())
+        currentPost.setText(this.postText);
+    }
 }
